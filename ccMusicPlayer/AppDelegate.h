291af19cc179d8062484f560ae2ac8c578fe0301
ccnyou <ccnyou@qq.com>
2013-12-11 11:08:27 +0800

    加入新的代码

---
+++ ccMusicPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ccMusicPlayer
+//
+//  Created by ccnyou on 12/5/13.
+//  Copyright (c) 2013 ccnyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ccMusicPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ccMusicPlayer
+//
+//  Created by ccnyou on 12/5/13.
+//  Copyright (c) 2013 ccnyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
